40ad48a7f43b1d1e48092fdcdcfd0eec5ead99be
andika666 <110449904+andika666@users.noreply.github.com>
2025-07-16 13:09:30 +0700

    Update api.js

--- api.js
+++ api.js
@@ -1,31 +1,7 @@
 async function fetchUsers() {
-  const res = await fetch("https://api.neynar.com/v2/farcaster/feed/global", {
-    headers: {
-      accept: "application/json",
-      api_key: "NEYNAR_API_DOCS",
-    },
-  });
+  const res = await fetch("/api/top");
   const data = await res.json();
-
-  const usersMap = {};
-  for (let cast of data.casts) {
-    const user = cast.author;
-    if (!usersMap[user.fid]) {
-      usersMap[user.fid] = {
-        username: user.username,
-        followers: user.followingCount,
-        profile: `https://warpcast.com/${user.username}`,
-        bio: user.profile?.bio?.text || "",
-        count: 1,
-      };
-    } else {
-      usersMap[user.fid].count++;
-    }
-  }
-
-  const topUsers = Object.values(usersMap)
-    .sort((a, b) => b.count - a.count || b.followers - a.followers)
-    .slice(0, 5);
+  const topUsers = data.topUsers;
 
   const container = document.getElementById("users");
   container.innerHTML = "";
